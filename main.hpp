6991976733cde80e2b68734251fc614d7ff8f532
adrianv24 <avskeleton@gmail.com>
2024-03-15 23:48:08 -0700

    testing

--- main.hpp
+++ main.hpp
@@ -1,3 +1,60 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+// main.hpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+int writeFile(const string& filename) {
+    ofstream file(filename);
+    if (!file) {
+        cerr << "Error opening file." << endl;
+         return 0;
+    }
+    int numEmployees;
+    cout << "Enter the number of employees: ";
+    cin >> numEmployees;
+    file << numEmployees << endl;
+    for (int i = 0; i < numEmployees; ++i) {
+        int id;
+        string name, department;
+        double salary;
+
+        
+        
+        cout << "Enter Employee ID, Name, Department, and Salary: ";
+        cin >> id >> name >> department >> salary;
+        file << id << " " << name << " " << department << " " << salary << endl;
+    }
+    cout << "Employee records have been written to " << filename << endl;
+    file.close();
+    return numEmployees;
+}
+
+int readFile(const string& filename) {
+    ifstream file(filename);
+    if (!file) {
+        cerr << "Error opening file." << endl;
+        return 0;
+    }
+
+    int totalEmployees = 0;
+    int numEmployees;
+    file >> numEmployees;
+
+    cout << "ID\tName\tDepartment\tSalary" << endl;
+    for (int i = 0; i < numEmployees; ++i) {
+        int id;
+        string name, department;
+        double salary;
+
+        file >> id >> name >> department >> salary;
+
+        totalEmployees++;
+
+        cout << id << "\t" << name << "\t" << department << "\t" << salary << endl;
+    }
+
+    file.close();
+    return totalEmployees;
+}
